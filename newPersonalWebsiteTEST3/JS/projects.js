9f653f61b7c61160207799dc55861c768a5c81e3
Bart C <bart.chr@gmail.com>
2016-08-27 22:19:32 -0400

    Add files via upload

--- newPersonalWebsiteTEST3/JS/projects.js
+++ newPersonalWebsiteTEST3/JS/projects.js
@@ -4,8 +4,9 @@ $(document).ready(function () {
     mobileFriendlyNavBar();
     //    projTileClick();
     $('#nav-icon3').click(function () {
-    $(this).toggleClass('open');
-});
+        $(this).toggleClass('open');
+    });
+    navColorChange();
 });
 
 $(window).resize(function () {
@@ -15,6 +16,7 @@ $(window).resize(function () {
     $('#nav-icon3').click(function () {
         $(this).toggleClass('open');
     });
+    navColorChange();
 });
 
 //$(function () {
@@ -56,13 +58,28 @@ function openNav() {
     console.log(openedClosedNav);
     if (openedClosedNav == false) {
         var windowHeight = window.innerHeight - 60;
+        $(".nav li").css("margin-top", windowHeight / 10);
         console.log(windowHeight);
         $(".collapsingNav").animate({
             height: windowHeight
         }, 300);
-        //        $("body").animate({
-        //            marginTop: windowHeight
-        //        }, 800);
+        $("#homeButton, #bioButton, #skillsButton, #projectsButton, #contactButton").css("opacity", "0");
+        $("#homeButton, #bioButton, #skillsButton, #projectsButton, #contactButton").removeClass("fadeOutRight");
+        setTimeout(function () {
+            $("#homeButton").addClass("fadeInRight")
+        }, 100);
+        setTimeout(function () {
+            $("#bioButton").addClass("fadeInRight")
+        }, 200);
+        setTimeout(function () {
+            $("#skillsButton").addClass("fadeInRight")
+        }, 300);
+        setTimeout(function () {
+            $("#projectsButton").addClass("fadeInRight")
+        }, 400);
+        setTimeout(function () {
+            $("#contactButton").addClass("fadeInRight")
+        }, 500);
 
         openedClosedNav = true;
         console.log(openedClosedNav);
@@ -75,6 +92,23 @@ function openNav() {
         //        }, 800);
         openedClosedNav = false;
         console.log(openedClosedNav);
+        $("#homeButton, #bioButton, #skillsButton, #projectsButton, #contactButton").css("opacity", "1");
+        $("#homeButton, #bioButton, #skillsButton, #projectsButton, #contactButton").removeClass("fadeInRight");
+        setTimeout(function () {
+            $("#homeButton").addClass("fadeOutRight")
+        }, 80);
+        setTimeout(function () {
+            $("#bioButton").addClass("fadeOutRight")
+        }, 60);
+        setTimeout(function () {
+            $("#skillsButton").addClass("fadeOutRight")
+        }, 40);
+        setTimeout(function () {
+            $("#projectsButton").addClass("fadeOutRight")
+        }, 20);
+        setTimeout(function () {
+            $("#contactButton").addClass("fadeOutRight")
+        }, 0);
     }
 }
 
@@ -105,72 +139,116 @@ function projTileClick(bool) {
 
 
 
+function navColorChange() {
+    $(function () {
+        $('#skills, #skillsButton').hover(function () {
+                $("#skills, #skillsButton").css("color", "#0058f1");
+                $("#projects, #projectsButton").css("color", "black");
+            },
+            function () {
+                $("#skills, #skillsButton").css("color", "black");
+                $("#projects, #projectsButton").css("color", "#0058f1");
+            });
 
-$(function () {
-    $('#skills').hover(function () {
-            $("#skills").css("color", "#0058f1");
-            $("#projects").css("color", "black");
-        },
-        function () {
-            $("#skills").css("color", "black");
-            $("#projects").css("color", "#0058f1");
-        });
+        $('#logo, #homeButton').hover(function () {
+                $("#logo").css("-webkit-filter", "grayscale(0%)");
+                $("#logo").css("filter", "grayscale(0%)");
+                $("#homeButton").css("color", "#0058f1");
+                $("#projects, #projectsButton").css("color", "black");
+            },
+            function () {
+                $("#logo").css("-webkit-filter", "grayscale(100%)");
+                $("#logo").css("filter", "grayscale(100%)");
+                $("#homeButton").css("color", "black");
+                $("#projects, #projectsButton").css("color", "#0058f1");
+            });
 
-    $('#logo').hover(function () {
-            $("#logo").css("-webkit-filter", "grayscale(0%)");
-            $("#logo").css("filter", "grayscale(0%)");
-            $("#projects").css("color", "black");
-        },
-        function () {
-            $("#logo").css("-webkit-filter", "grayscale(100%)");
-            $("#logo").css("filter", "grayscale(100%)");
-            $("#projects").css("color", "#0058f1");
-        });
+        $('#bio, #bioButton').hover(function () {
+                $("#bio, #bioButton").css("color", "#0058f1");
+                $("#projects, #projectsButton").css("color", "black");
+            },
+            function () {
+                $("#bio, #bioButton").css("color", "black");
+                $("#projects, #projectsButton").css("color", "#0058f1");
+            });
 
-    $('#projects').hover(function () {
-            $("#projects").css("color", "#0058f1");
-            $("#projects").css("color", "black");
-        },
-        function () {
-            $("#projects").css("color", "black");
-            $("#projects").css("color", "#0058f1");
-        });
-
-    $('#contact').hover(function () {
-            $("#contact").css("color", "#0058f1");
-            $("#projects").css("color", "black");
-        },
-        function () {
-            $("#contact").css("color", "black");
-            $("#projects").css("color", "#0058f1");
-        });
-});
+        $('#contact, #contactButton').hover(function () {
+                $("#contact, #contactButton").css("color", "#0058f1");
+                $("#projects, #projectsButton").css("color", "black");
+            },
+            function () {
+                $("#contact, #contactButton").css("color", "black");
+                $("#projects, #projectsButton").css("color", "#0058f1");
+            });
+        //    $('#skillsButton').hover(function () {
+        //            $("#skillsButton").css("color", "#0058f1");
+        //            $("#projectsButton").css("color", "black");
+        //        },
+        //        function () {
+        //            $("#skillsButton").css("color", "black");
+        //            $("#projectsButton").css("color", "#0058f1");
+        //        });
+        //
+        //    $('#homeButton').hover(function () {
+        //            $("#homeButton").css("color", "#0058f1");
+        //            $("#projectsButton").css("color", "black");
+        //        },
+        //        function () {
+        //            $("#homeButton").css("color", "black");
+        //            $("#projectsButton").css("color", "#0058f1");
+        //        });
+        //
+        //    $('#bioButton').hover(function () {
+        //            $("#bioButton").css("color", "#0058f1");
+        //            $("#projectsButton").css("color", "black");
+        //        },
+        //        function () {
+        //            $("#bioButton").css("color", "black");
+        //            $("#projectsButton").css("color", "#0058f1");
+        //        });
+        //
+        //    $('#contactButton').hover(function () {
+        //            $("#contactButton").css("color", "#0058f1");
+        //            $("#projectsButton").css("color", "black");
+        //        },
+        //        function () {
+        //            $("#contactButton").css("color", "black");
+        //            $("#projectsButton").css("color", "#0058f1");
+        //        });
+    });
+};
 
 
 /*NavBar Disappear*/
 var lastScrollTop = 0;
 $(window).scroll(function (event) {
-    var scrollNum = $(this).scrollTop();
-    //    console.log($(".collapsingNav").css("height"));
-    //    console.log((window.innerHeight - 60).toString() + "px");
-    if ($(".collapsingNav").css("height") == (window.innerHeight - 60).toString() + "px") {
-        $("#navBarM").removeClass("animated");
-    } else {
-        $("#navBarM").addClass("animated");
-    }
 
-    if (scrollNum > lastScrollTop) {
-        $("#navBar").removeClass("slideInDown");
-        $("#navBar").addClass("slideOutUp");
-        $("#navBarM").removeClass("slideInDown");
-        $("#navBarM").addClass("slideOutUp");
-    } else {
-        $("#navBar").removeClass("slideOutUp");
-        $("#navBar").addClass("slideInDown");
-        $("#navBarM").removeClass("slideOutUp");
-        $("#navBarM").addClass("slideInDown");
+    var windowHeight = ((window.innerHeight - 60) + "px").toString();
+    var navHeight = $(".collapsingNav").css("height");
+
+    if (windowHeight != navHeight) {
+        var scrollNum = $(this).scrollTop();
+        //    console.log($(".collapsingNav").css("height"));
+        //    console.log((window.innerHeight - 60).toString() + "px");
+        if ($(".collapsingNav").css("height") == (window.innerHeight - 60).toString() + "px") {
+            $("#navBarM").removeClass("animated");
+        } else {
+            $("#navBarM").addClass("animated");
+        }
+
+        if (scrollNum > lastScrollTop) {
+            $("#navBar").removeClass("slideInDown");
+            $("#navBar").addClass("slideOutUp");
+            $("#navBarM").removeClass("slideInDown");
+            $("#navBarM").addClass("slideOutUp");
+        } else {
+            $("#navBar").removeClass("slideOutUp");
+            $("#navBar").addClass("slideInDown");
+            $("#navBarM").removeClass("slideOutUp");
+            $("#navBarM").addClass("slideInDown");
+        }
+        lastScrollTop = scrollNum;
     }
-    lastScrollTop = scrollNum;
 });
 
 //$(".collapsingNav").css("height") == (window.innerHeight - 60).toString() + "px"
@@ -362,4 +440,4 @@ var projectTilesD = '<div id="center">' +
 var contactWindow = '<div style="padding:0% 2% 2% 2%"><h1 style="font-family: Quatro Slab; font-weight: bold; font-size: 3em">Hi There!<a onclick="closeContactWindow()"><span class="crossClose"> </span></a></h1><p style="font-size: 0.90em; line-height: 1.2em">Interested in working with me on a project? Or take me with you into any of your business ventures? Or just curious? Whatever it is, lets get in touch!</p><p style="margin-top: 3%; font-weight: 600; font-size:1.5em">bart.chr@gmail.com</p><p style="font-weight: 600; font-size:1.5em">bchrzasz@edu.uwaterloo.ca</p><div style="margin-top: 5%; font-size: 1.5em"><span><a style="padding-right: 5%" class="contactLinks" href="https://github.com/bartchr808"><i class="fa fa-github"></i></a></span><span><a style="padding-right: 5%" class="contactLinks" href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=bart.chr@gmail.com&tf=1"><i class="fa fa-envelope"></i></a></span><span><a style="padding-right: 5%" class="contactLinks" href="https://codepen.io/bartchr808/pens/public/"><i class="fa fa-codepen"></i></a></span><span><a style="padding-right: 5%" class="contactLinks" href="https://www.linkedin.com/in/bartlomiej-c-1222a010a?trk=hp-identity-name"><i class="fa fa-linkedin"></i></a></span></div></div>'
 
 var navBarD = '<div class="row"><div class="col-sm-2 col-sm-offset-1"><p id="navBarText"><a id="bio" class="navLinks" href="../HTML/biography.html">Biography</a></p></div><div class="col-sm-2"><p id="navBarText"><a id="skills" class="navLinks" href="../HTML/skills.html">Skills</a></p></div><div class="col-sm-2"><a href="home.html"><img class="activeTab" id="logo" src="../Pictures/bartLogo.png"></a></div><div class="col-sm-2"><p id="navBarText"><a id="projects" style="color: #0058f1" class="navLinks" href="../HTML/projects.html">Projects</a></p></div><div class="col-sm-2"><p id="navBarText"><a id="contact" class="navLinks" onclick="openContactWindow()">Contact</a></p></div><div class="col-sm-1"></div></div>'
-var navBarM = '<div class="row"><div class="col-sm-4"><div class="sidebar-nav"><div id="navBarM" style="padding: 0 0 0 0; margin: 0 0 0 0" class="animated navbar navbar-default navbar-fixed-top" role="navigation"><div class="navbar-header"><img style="margin: 0% 0 0 2%; height: 60px; width: auto; float:left" src="../Pictures/bartLogo.png"><div onclick="openNav()" id="nav-icon3"><span></span><span></span><span></span><span></span></div></div><div class="collapsingNav"><ul class="nav navbar-nav"><li class="active"><a href="../HTML/home.html">Home</a></li><li><a href="../HTML/biography.html">Bio</a></li><li><a href="../HTML/skills.html">Skills</a></li><li><a href="../HTML/projects.html">Projects</a></li><li><a href="#">Contact</a></li></ul></div></div></div></div></div>'
+var navBarM = '<div class="row"><div class="col-sm-4"><div class="sidebar-nav"><div id="navBarM" style="padding: 0 0 0 0; margin: 0 0 0 0" class="animated navbar navbar-default navbar-fixed-top" role="navigation"><div class="navbar-header"><img style="margin: 0% 0 0 2%; height: 60px; width: auto; float:left" src="../Pictures/bartLogo.png"><div onclick="openNav()" id="nav-icon3"><span></span><span></span><span></span><span></span></div></div><div class="collapsingNav"><ul class="nav navbar-nav"><li><a id="homeButton" class="navLinks animated" href="../HTML/home.html">Home</a></li><li><a id="bioButton" class="animated" href="../HTML/biography.html">Bio</a></li><li><a id="skillsButton" class="animated" href="../HTML/skills.html">Skills</a></li><li><a style="color: #0058f1" id="projectsButton" class="animated" href="../HTML/projects.html">Projects</a></li><li><a id="contactButton" class="animated" href="#">Contact</a></li></ul></div></div></div></div></div>'
